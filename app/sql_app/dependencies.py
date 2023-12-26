5a0d15104f950c43748786867369f2b3c6af829b
gonzalo9991r <go.rojasf@duocuc.cl>
2023-12-26 16:16:17 -0300

    ADD: First Commit

---
+++ app/sql_app/dependencies.py
@@ -0,0 +1,10 @@
+from app.sql_app.database import SessionLocal
+
+
+# Dependencia para usar en las rutas.
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
