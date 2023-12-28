947a8dc9b7f31be93bb9d609fb411ef53c5f512a
gonzalo9991r <go.rojasf@duocuc.cl>
2023-12-27 21:06:49 -0300

    ADD: new changes to models and schemas

--- app/sql_app/crud.py
+++ app/sql_app/crud.py
@@ -16,21 +16,28 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
+    print(user)
     fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db_user = models.User(
+        email=user.email,
+        username=user.username,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        hashed_password=fake_hashed_password,
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
 
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
+def get_tasks(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Task).offset(skip).limit(limit).all()
 
 
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
+def create_user_task(db: Session, task: schemas.TaskCreate, user_id: int):
+    db_task = models.Task(**task.dict(), owner_id=user_id)
+    db.add(db_task)
     db.commit()
-    db.refresh(db_item)
-    return db_item
+    db.refresh(db_task)
+    return db_task
--- app/sql_app/models.py
+++ app/sql_app/models.py
@@ -4,23 +4,41 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 
-class User(Base):
+class Generic(Base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, index=True)
+    status = Column(String, index=True)
+    created_at = Column(String, index=True, default="now()")
+    updated_at = Column(String, index=True)
+    deleted_at = Column(String, index=True)
+
+
+class User(Generic):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    username = Column(String, index=True, unique=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
+    tasks = relationship("Task", back_populates="owner")
 
 
-class Item(Base):
-    __tablename__ = "items"
-
+class Task(Generic):
+    __tablename__ = "tasks"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="tasks")
+    categories = relationship("Category", back_populates="task")
 
-    owner = relationship("User", back_populates="items")
+
+class Category(Generic):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    task_id = Column(Integer, ForeignKey("tasks.id"))
+    task = relationship("Task", back_populates="categories")
