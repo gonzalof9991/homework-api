0a02c8a07caee8e9969e3c7b7dbd5d0f5b719f13
gonzalo9991r <go.rojasf@duocuc.cl>
2024-01-15 16:39:24 -0300

    FIX: Update CI v2

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -2,9 +2,9 @@ def get_url_database() -> str:
     import os
     from dotenv import load_dotenv
     load_dotenv()
-    user = os.environ.get("POSTGRES_USER")
-    password = os.environ.get("POSTGRES_PASSWORD")
-    host = os.environ.get("POSTGRES_HOST")
-    db = os.environ.get("POSTGRES_DB")
-    port = os.environ.get("POSTGRES_PORT")
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("POSTGRES_HOST")
+    db = os.getenv("POSTGRES_DB")
+    port = os.getenv("POSTGRES_PORT")
     return f"postgresql://{user}:{password}@{host}:{port}/{db}"
